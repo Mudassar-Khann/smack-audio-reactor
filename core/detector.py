0ad078d49da7f3ac66e68714196b52677db448b1
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-11 14:37:37 +0500

    refactor: fix typo and improve code readability in ImpactDetector class

--- core/detector.py
+++ core/detector.py
@@ -1,17 +1,11 @@
-class ImpactDetactor:
-    def __init__(self, hthreshold, lthreshold):
+class ImpactDetector:
+    def __init__(self, high_threshold, low_threshold):
+        self.high_threshold = high_threshold
+        self.low_threshold = low_threshold
 
-        self.high_threshold = hthreshold
-        self.low_threshold = lthreshold
-
-    def detect(self, volume ):
-        if volume > self.high_threshold:
+    def detect(self, volume):
+        if volume >= self.high_threshold:
             return "high"
-        elif volume > self.low_threshold and volume < self.high_threshold:
+        elif volume >= self.low_threshold:
             return "low"
-
-        else: return None
-
-
-
-
+        return None
