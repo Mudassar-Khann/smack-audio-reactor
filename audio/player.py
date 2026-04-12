4df47e882463f3483fecd5e1924c858a01e45e08
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-12 14:58:01 +0500

    reafctor: Initialize pygame mixer once and load sounds at class level

--- audio/player.py
+++ audio/player.py
@@ -3,21 +3,30 @@ from pathlib import Path
 
 class SoundPlayer:
 
+    _initialized = False
 
-    def paly(hit):
+    BASE_DIR = Path(__file__).resolve().parents[2]
+    path = BASE_DIR / "assets" / "sounds"
+
+    sounds = {i.stem: str(i) for i in path.iterdir() if i.is_file()}
+
+
+    @classmethod
+    def init(cls):
+        if not cls._initialized:
+            pygame.mixer.init()
+            cls._initialized = True
+
+
+    def play(cls, hit):
         if hit:
-            base = Path(__file__).parent
-            path = base.parent / "aasets" / "sounds"
             if hit == "soft":
-                sound = pygame.mixer.Sound(path.write_text("anime-ahh.wav"))
+                sound = pygame.mixer.Sound(cls.sounds["anime-ahh"])
 
             elif hit == "hard":
-                sound = pygame.mixer.Sound(path.write_text("yamate-kudesai.wav"))
+                sound = pygame.mixer.Sound(cls.sounds["yamate-kudesai"])
 
             return sound.play()
-
-
-
 
 
 
