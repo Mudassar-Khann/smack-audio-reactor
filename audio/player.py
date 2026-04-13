ce71577954699ac4c5fcb25d28bafd0f8d676d6e
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-13 15:09:35 +0500

    refactor: chnaged the sound implementation to  manager,py

--- audio/player.py
+++ audio/player.py
@@ -1,15 +1,16 @@
 import pygame
 from pathlib import Path
-import time
+
 class SoundPlayer:
     _initialized = False
 
     def __init__(self):
         SoundPlayer.init()
+
         base = Path(__file__).resolve().parents[1]
         sounds_path = base / "assets" / "sounds"
 
-
+        # preload sounds
         self._sounds = {
             f.stem: pygame.mixer.Sound(str(f))
             for f in sounds_path.iterdir()
@@ -22,16 +23,13 @@ class SoundPlayer:
             pygame.mixer.init()
             cls._initialized = True
 
-    def play(self, hit: str | None):
-        if hit is None:
+    def play(self, sound_name: str | None):
+        if sound_name is None:
             return
-        sound_map = {
-            "soft": "modi-ji-bkl",
-            "hard": "yamate-kudesai"
-        }
-        name = sound_map.get(hit)
-        if name and name in self._sounds:
-            self._sounds[name].play()
-        else:
-            print(f"[WARN] No sound mapped for hit='{hit}'")
 
+        sound = self._sounds.get(sound_name)
+
+        if sound:
+            sound.play()
+        else:
+            print(f"[WARN] Sound '{sound_name}' not found")
