0e063b64201cdb270d33fb2920bd5ebbf1da27df
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-14 09:46:52 +0500

    feat: Add main application logic with audio listener and event queue

--- main.py
+++ main.py
@@ -1,30 +1,67 @@
-import sounddevice as sd
-import numpy as np
 from core.detector import ImpactDetector
 from audio.player import SoundPlayer
 from audio.manager import SoundManager
+from core.listener import MicListener
+from config import Config, SETTINGS
 
-# low_threshold  = 0.050
-# high_threshold = 0.088
+from queue import Queue
+import threading
+import logging
 
-imp = ImpactDetector(
-    low_threshold  = 0.055,
-    high_threshold = 0.15,
-    spike_threshold = 0.045,
-    cooldown = 1.5
+
+# load detector config
+det_cfg = SETTINGS["detector"]
+
+detector = ImpactDetector(
+    low_threshold=det_cfg["low_threshold"],
+    high_threshold=det_cfg["high_threshold"],
+    spike_threshold=det_cfg["spike_threshold"],
+    cooldown=det_cfg["cooldown"]
 )
-sp  = SoundPlayer()
 
+player = SoundPlayer()
 manager = SoundManager()
 
-def callback(indata, frames, time_info, status):
-    volume = np.abs(indata).mean()
 
-    impact = imp.detect(volume)
+event_queue = Queue()
 
-    sound = manager.get_fixed_sound(impact)
 
-    sp.play(sound)
+def on_volume(volume):
+    """
+    Runs in audio thread (via listener)
 
-with sd.InputStream(callback=callback, channels=1, samplerate=44100, blocksize=1024):
-    input("Listening... press Enter to stop\n")
+    """
+    impact = detector.detect(volume)
+
+    if impact:
+        event_queue.put(impact)
+
+
+def worker():
+
+    while True:
+        impact = event_queue.get()
+
+        sound = manager.get_fixed_sound(impact)
+        player.play(sound)
+
+        if Config.DEBUG:
+            print(f"[DEBUG] {impact}")
+            logging.info(f"Impact: {impact}")
+
+
+threading.Thread(target=worker, daemon=True).start()
+
+
+listener = MicListener(
+    samplerate=Config.SAMPLE_RATE,
+    channels=Config.CHANNELS,
+    blocksize=Config.BLOCK_SIZE,
+    callback=on_volume
+)
+
+listener.start()
+
+input("Listening... press Enter to stop\n")
+
+listener.stop()
